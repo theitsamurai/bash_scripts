251b33e5f5d24c581e8c7b6db89ee3f0808d9ba4
theitsamurai <63486488+theitsamurai@users.noreply.github.com>
2020-10-09 09:49:45 -0400

    Added documentation

--- sha512.sh
+++ sha512.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+# Prompts user to enter a string:
 echo Enter string to hash with sha512:
 
+# Gets the string that the user enters
 read var
 
+# echo -n: the -n is necessary as echo normally prints
+# a newline character at the end of output which will
+# cause the hash value to be different.
+#
+# shasum -a 512: hashes the echoed string using sha512
 echo -n $var | shasum -a 512
