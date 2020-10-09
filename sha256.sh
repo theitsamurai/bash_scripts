7c85b8a6cf6556c61db2921a9c111c1a5ae1124a
theitsamurai <63486488+theitsamurai@users.noreply.github.com>
2020-10-09 09:48:32 -0400

    Added documentation

--- sha256.sh
+++ sha256.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-echo Enter string to hash with sha256:
+# Prompts user to enter a string:
+echo Enter a string to hash with sha256:
 
+# Gets the string that the user enters
 read var
 
+# echo -n: the -n is necessary as echo normally prints
+# a newline character at the end of output which will
+# cause the hash value to be different.
+#
+# shasum -a 256: hashes the echoed string using sha256
 echo -n $var | shasum -a 256
